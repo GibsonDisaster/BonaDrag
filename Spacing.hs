e67e07131ea4a548e3165cc55eb1870e86ee9c5c
HenningTonko <henninghtonko@gmail.com>
2018-08-31 21:42:03 -0500

    finished spacing functions

--- Spacing.hs
+++ Spacing.hs
@@ -1,5 +1,6 @@
 module Spacing where
   import Types
+  import Data.Bifunctor
 
   {-
 
@@ -38,49 +39,45 @@ module Spacing where
       newEndY = (snd . bottomRight) sec2 - height sec2
   
   besideR :: Section a -> Section a -> Section a
-  besideR sec1 sec2 = sec1
+  besideR sec1 sec2 = sec1 { topLeft = (newStartX, newStartY), bottomRight = (newEndX, newEndY) }
     where
       newStartX = (fst . topLeft) sec2 + width sec2
       newStartY = (snd . topLeft) sec2
-      newEndX = (fst . bottomRight) sec2 + width sec1
-      newEndY = (snd . bottomRight) sec2
+      newEndX = newStartX + width sec1
+      newEndY = newStartY + height sec1
 
   besideL :: Section a -> Section a -> Section a
-  besideL sec1 sec2 = sec1
+  besideL sec1 sec2 = sec1 { topLeft = (newStartX, newStartY), bottomRight = (newEndX, newEndY) }
     where
-      newStartX = (fst . topLeft) sec2
-      newStartY = (snd . topLeft) sec2 + height sec2
-      newEndX = (fst . bottomRight) sec2
-      newEndY = (snd . bottomRight) sec2 + height sec1
+      newStartX = (fst . topLeft) sec2 - width sec1
+      newStartY = (snd . topLeft) sec2
+      newEndX = newStartX + width sec1
+      newEndY = newStartY + height sec1
 
   belowN :: Section a -> Int -> Section a -> Section a
-  belowN sec1 n sec2 = sec1
+  belowN sec1 n sec2 = moveBelow { topLeft = bimap id ((+) n) newStart, bottomRight = bimap id ((+) n) newEnd }
     where
-      newStartX = (fst . topLeft) sec2
-      newStartY = (snd . topLeft) sec2 + height sec2
-      newEndX = (fst . bottomRight) sec2
-      newEndY = (snd . bottomRight) sec2 + height sec1
+      moveBelow = sec1 `below` sec2
+      newStart = topLeft moveBelow
+      newEnd = bottomRight moveBelow
 
   aboveN :: Section a -> Int -> Section a -> Section a
-  aboveN sec1 n sec2 = sec1
+  aboveN sec1 n sec2 = moveAbove { topLeft = bimap id (\y -> y - n) newStart, bottomRight = bimap id (\y -> y - n) newEnd }
     where
-      newStartX = (fst . topLeft) sec2
-      newStartY = (snd . topLeft) sec2 + height sec2
-      newEndX = (fst . bottomRight) sec2
-      newEndY = (snd . bottomRight) sec2 + height sec1
+      moveAbove = sec1 `above` sec2
+      newStart = topLeft moveAbove
+      newEnd = bottomRight moveAbove
 
   besideRN :: Section a -> Int -> Section a -> Section a
-  besideRN sec1 n sec2 = sec1
+  besideRN sec1 n sec2 = movetoRight { topLeft = bimap ((+) n) id newStart, bottomRight = bimap ((+) n) id newEnd }
     where
-      newStartX = (fst . topLeft) sec2
-      newStartY = (snd . topLeft) sec2 + height sec2
-      newEndX = (fst . bottomRight) sec2
-      newEndY = (snd . bottomRight) sec2 + height sec1
+      movetoRight = sec1 `besideR` sec2
+      newStart = topLeft movetoRight
+      newEnd = bottomRight movetoRight
       
   besideLN :: Section a -> Int -> Section a -> Section a
-  besideLN sec1 n sec2 = sec1
+  besideLN sec1 n sec2 = movetoLeft { topLeft = bimap (\y -> y - n) id newStart, bottomRight = bimap (\y -> y - n) id newEnd }
     where
-      newStartX = (fst . topLeft) sec2
-      newStartY = (snd . topLeft) sec2 + height sec2
-      newEndX = (fst . bottomRight) sec2
-      newEndY = (snd . bottomRight) sec2 + height sec1
+      movetoLeft = sec1 `besideL` sec2
+      newStart = topLeft movetoLeft
+      newEnd = bottomRight movetoLeft
